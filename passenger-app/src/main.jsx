eb9985ad7a62262a40a53d4ab19e5925a91de614
udbhav07 <udbhavsai.k@gmail.com>
2026-05-07 06:46:57 +0530

    s

--- passenger-app/src/main.jsx
+++ passenger-app/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'leaflet/dist/leaflet.css';
 import { SocketProvider } from './context/SocketContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
